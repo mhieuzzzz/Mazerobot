7a45a68d0546348f80aee451f47545b099ec26d7
mhieuzzzz <iamminhhieu206@gmail.com>
2025-11-06 22:10:15 +0700

    Create motor.c

---
+++ motor.c
@@ -0,0 +1,22 @@
+#include "motor.h"
+#include "tim.h"
+
+void set_motor_pwm(uint16_t left_pwm, uint16_t right_pwm)
+{
+    // TODO: set TIM3 PWM cho motor trái và phải
+}
+
+void move_forward(void)
+{
+    // TODO: motor chạy tiến
+}
+
+void turn_left(void)
+{
+    // TODO: motor quay trái
+}
+
+void turn_right(void)
+{
+    // TODO: motor quay phải
+}
