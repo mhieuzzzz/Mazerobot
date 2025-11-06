d289037740f17fdad5abd93b629bcc81376cfb7e
mhieuzzzz <iamminhhieu206@gmail.com>
2025-11-06 22:12:47 +0700

    Create flash.c

---
+++ flash.c
@@ -0,0 +1,13 @@
+#include "flash.h"
+#include "maze.h"
+#include "stm32f4xx_hal.h"
+
+void save_maze_to_flash(void)
+{
+    // TODO: unlock, erase sector, ghi maze[][]
+}
+
+void load_maze_from_flash(void)
+{
+    // TODO: đọc dữ liệu maze từ flash
+}
