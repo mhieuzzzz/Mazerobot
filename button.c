f3f859b2406f5a35addcf5522f2324761504a088
mhieuzzzz <iamminhhieu206@gmail.com>
2025-11-06 22:28:31 +0700

    Update button.c

--- button.c
+++ button.c
@@ -1,7 +1,17 @@
 #include "button.h"
-#include "gpio.h"
+#include "main.h"
 
+/* Assumes PA10 configured as input with pull-up (pressed = 0) */
 void wait_button(void)
 {
-    while(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10) == GPIO_PIN_SET);
+    // simple debounce
+    while(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10) == GPIO_PIN_SET) {
+        HAL_Delay(10);
+    }
+    // wait for release
+    HAL_Delay(50);
+    while(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10) == GPIO_PIN_RESET) {
+        HAL_Delay(10);
+    }
+    HAL_Delay(50);
 }
