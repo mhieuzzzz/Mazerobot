8f7f96607efaf6e946b1ad93632a43d917a37b4a
mhieuzzzz <iamminhhieu206@gmail.com>
2025-11-06 22:50:11 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,52 +1,71 @@
-#include "main.h"
 #include "maze.h"
-#include "ir_sensor.h"
-#include "motor.h"
-#include "encoder.h"
-#include "fast_run.h"
 #include "flash.h"
-#include "button.h"
-#include "buzzer_led.h"
+#include "motor.h"
+#include "ir_sensor.h"
+#include "fast_run.h"
+#include "main.h"
 
-ADC_HandleTypeDef hadc1;
-TIM_HandleTypeDef htim2;
-TIM_HandleTypeDef htim3;
+#define MAZE_SIZE 16
 
-int main(void)
+Cell maze[MAZE_SIZE][MAZE_SIZE];
+uint8_t current_x = 0, current_y = 0;
+uint8_t direction = 0; // 0: North, 1: East, 2: South, 3: West
+
+void maze_init(void)
 {
-    HAL_Init();
-    SystemClock_Config();
+    for (int y = 0; y < MAZE_SIZE; y++)
+        for (int x = 0; x < MAZE_SIZE; x++)
+        {
+            maze[y][x].wall_north = 0;
+            maze[y][x].wall_east  = 0;
+            maze[y][x].wall_south = 0;
+            maze[y][x].wall_west  = 0;
+            maze[y][x].visited    = 0;
+        }
+}
 
-    MX_GPIO_Init();
-    MX_ADC1_Init();
-    MX_TIM2_Init();
-    MX_TIM3_Init();
-
-    /* Start PWM timers if needed */
-    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1); // Depending on Cube config
-    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
-    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
-    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_4);
-
-    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1); // motor left
-    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2); // motor right
-
-    // Optional: init encoder interrupts or timers (encoder.c handles)
-    encoder_init();
-
-    // Chờ người dùng ấn nút PA10 để bắt đầu
-    wait_button();
-
-    // Lần 1: học map
-    learn_maze();
-    save_maze_to_flash();
-
-    // Lần 2: chạy nhanh
-    load_maze_from_flash();
-    fast_run();
+void learn_maze(void)
+{
+    maze_init();
+    current_x = 0;
+    current_y = 0;
+    direction = 0; // bắt đầu hướng Bắc
 
     while (1)
     {
-        HAL_Delay(1000);
+        read_ir_sensors();
+        maze[current_y][current_x].visited = 1;
+
+        // Giả lập đọc tường (thực tế đọc giá trị ADC IR)
+        if (front_wall_detected())  maze[current_y][current_x].wall_north = 1;
+        if (left_wall_detected())   maze[current_y][current_x].wall_west = 1;
+        if (right_wall_detected())  maze[current_y][current_x].wall_east = 1;
+
+        // di chuyển sang ô mới
+        motor_forward(50);
+        HAL_Delay(500);
+        motor_stop();
+
+        current_y++;
+        if (current_y >= MAZE_SIZE - 1) break; // dừng học đơn giản
+
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13); // debug LED
     }
+
+    save_maze_to_flash();
+}
+
+void save_maze_to_flash(void)
+{
+    HAL_StatusTypeDef status;
+    status = Flash_Erase_Sector(FLASH_SECTOR_USED);
+    if (status == HAL_OK)
+    {
+        Flash_Write_Array(FLASH_SECTOR_ADDR, (uint8_t *)maze, sizeof(maze));
+    }
+}
+
+void load_maze_from_flash(void)
+{
+    Flash_Read_Array(FLASH_SECTOR_ADDR, (uint8_t *)maze, sizeof(maze));
 }
