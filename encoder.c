d28e8ee63c6d7a7fc3f7c3baca3c40d84053a8f7
mhieuzzzz <iamminhhieu206@gmail.com>
2025-11-06 22:10:46 +0700

    Create encoder.c

---
+++ encoder.c
@@ -0,0 +1,10 @@
+#include "encoder.h"
+#include "gpio.h"
+
+int32_t left_encoder_count = 0;
+int32_t right_encoder_count = 0;
+
+void encoder_update(void)
+{
+    // TODO: đọc encoder PB6, PB7, PB3, PA15
+}
