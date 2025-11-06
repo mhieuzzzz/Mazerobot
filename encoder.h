a36e40182759bd79200283217830eacda3136c80
mhieuzzzz <iamminhhieu206@gmail.com>
2025-11-06 22:25:24 +0700

    Update encoder.h

--- encoder.h
+++ encoder.h
@@ -1,11 +1,11 @@
 #ifndef ENCODER_H
 #define ENCODER_H
-
 #include "main.h"
 
-extern int32_t left_encoder_count;
-extern int32_t right_encoder_count;
+extern volatile int32_t left_encoder_count;
+extern volatile int32_t right_encoder_count;
 
+void encoder_init(void);
 void encoder_update(void);
 
-#endif
+#endif // ENCODER_H
