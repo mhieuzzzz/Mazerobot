8cabe90ff256cd4a26670d9ae40f7e29a48e6f6e
mhieuzzzz <iamminhhieu206@gmail.com>
2025-11-06 22:21:51 +0700

    Update maze.h

--- maze.h
+++ maze.h
@@ -1,6 +1,8 @@
 #ifndef MAZE_H
 #define MAZE_H
 
+#include "main.h"
+
 #define MAZE_SIZE 16
 #define EMPTY 0
 #define WALL 1
@@ -8,10 +10,10 @@
 
 extern uint8_t maze[MAZE_SIZE][MAZE_SIZE];
 extern uint8_t robot_x, robot_y;
-extern uint8_t robot_dir;
+extern uint8_t robot_dir; // 0: up,1:right,2:down,3:left
 
 void learn_maze(void);
 void update_maze_map(void);
 int goal_reached(void);
 
-#endif
+#endif // MAZE_H
