3ac3e4bd992b2ef57004f3ab7bad0bc5fbb0dd14
mhieuzzzz <iamminhhieu206@gmail.com>
2025-11-06 22:24:35 +0700

    Update motor.h

--- motor.h
+++ motor.h
@@ -1,11 +1,17 @@
 #ifndef MOTOR_H
 #define MOTOR_H
-
 #include "main.h"
 
+#define MOTOR_PWM_MAX 1000
+
+void set_motor_pwm(uint16_t left_pwm, uint16_t right_pwm);
+void stop_motors(void);
 void move_forward(void);
+void move_backward(void);
 void turn_left(void);
 void turn_right(void);
-void set_motor_pwm(uint16_t left_pwm, uint16_t right_pwm);
 
-#endif
+/* optional: PID loop update */
+void motor_pid_update(void);
+
+#endif // MOTOR_H
